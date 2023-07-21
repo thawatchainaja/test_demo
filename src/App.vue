9b5b3d8e0a80bf7bb2096a7b95b88faba22d0a4a
Administrator <thawatchai.no@inet.co.th>
2023-07-21 16:38:34 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app class="web-bg">
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  })
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
+  .web-bg {
+    background: #c5c6c9 !important;
+  }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/views/Toolbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <v-container fluid class="pa-0">
+            <v-toolbar>
+                asd
+            </v-toolbar>
+        </v-container>
+    </div>
+</template>
+
+<style>
+    
+    
+
+</style>
